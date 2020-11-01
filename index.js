1380ac8d0da4abd0ffffb54e96ba1c745bc3ce0f
rawan <rawan.designer@hotmail.com>
2020-11-01 02:39:50 +0200

    Add the sloution of fizzbuzz and the test

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 'use strict';
 
-function fizzbuzz() {
+function fizzbuzz(num) {
   // write your code here
-}
+  let result 
+    if(num % 3 === 0 && num % 5 === 0){
+      result = "fizzbuzz"
+    } else if( num % 5 === 0){
+      result =  "buzz"
+    } else if(num %3 ===0 ){
+      result =  'fizz'
+    } else{
+    result = num
+    }
+  return result
+  }
+
+
+console.log(fizzbuzz(4))
 
 module.exports = fizzbuzz;
--- test.js
+++ test.js
@@ -3,3 +3,37 @@ var fizzbuzz = require('./index.js');
 test('Testing Jest is working', function() {
 	expect(2 + 2).toBe(4);
 });
+
+describe('Testing fizzbuzz return value', () => {
+	test('Should return number when the value is not a multiple of 3 or 5', () => {
+		var actual = fizzbuzz(2);
+		var expected = 2;
+		expect(actual).toBe(expected);
+	});
+});
+
+describe('Testing fizzbuzz return value', () => {
+	test('Should return fizz when the value is a multiple of 3' , () => {
+		var actual = fizzbuzz(3);
+		var expected = 'fizz';
+		expect(actual).toBe(expected);
+	});
+});
+
+describe('Testing fizzbuzz return value', () => {
+	test('Should return buzz when the value is a multiple of 5', () => {
+		var actual = fizzbuzz(5);
+		var expected = 'buzz';
+		expect(actual).toBe(expected);
+	});
+});
+
+
+describe('Testing fizzbuzz return value', () => {
+	test('Should return fizzbuzz when the value is  a multiple of 3 and 5', () => {
+		var actual = fizzbuzz(15);
+		var expected = 'fizzbuzz';
+		expect(actual).toBe(expected);
+	});
+});
+
